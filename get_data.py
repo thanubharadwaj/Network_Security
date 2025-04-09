1752f9a9789b62d45a0fe98afb8f96360d02a25d
Thanushree Sridhara.py <bharadwajthanu25@gmail.com>
2025-04-09 16:36:41 +0530

    get_data.py is updated

--- get_data.py
+++ get_data.py
@@ -35,12 +35,22 @@ class NetworkDataExtract():
         
     def pushing_data_to_mongodb(self,records, database,collection):
         try:
-            pass
+            self.database = database
+            self.collection = collection
+            self.records = records
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
 if __name__  =='__main__':
     FILE_PATH = "./Network_Data/NetworkData.csv"
+    DATABASE = "Networksecpro"
+    COLLECTION = "Networkdata"
     networkobj = NetworkDataExtract()
     records = networkobj.csv_to_json_convertor(FILE_PATH)
-    print(records)
+    no_of_records = networkobj.pushing_data_to_mongodb(records,DATABASE,COLLECTION)
+    print(no_of_records)
